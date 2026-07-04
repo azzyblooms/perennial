56008e1ebc6d74f99415723383a8508b6f4b47a6
azzyblooms <azzyblooms@gmail.com>
2026-07-03 22:38:34 -0400

    added some easter eggs and stuff. will be a while before its important.

--- music.js
+++ music.js
@@ -3,6 +3,8 @@ const musicbigbox = document.getElementById("musicbigbox")
 const hoversound = new Audio('audio/hover.mp3')
 const clicksound = new Audio('audio/click2.mp3')
 const boom = new Audio ('audio/boom.mp3')
+const catcollect = new Audio ('audio/catcollect.wav')
+const mew = new Audio ('audio/mew.wav')
 const ihover = document.getElementById("ihover")
 const sidebox = document.getElementById("sidebox")
 const closeside = document.getElementById("closeside")
@@ -11,6 +13,7 @@ const abttsimg = document.getElementById("abttsimg")
 const ititbox = document.getElementById("ititbox")
 const abtyap = document.getElementById("abtyap")
 const doodlebcl = document.getElementById("doodlebcl")
+const doodlecat = document.getElementById("doodlecat")
 
 const flt = document.getElementById("flt")
 const bcl = document.getElementById("bcl")
@@ -153,6 +156,7 @@ pno.addEventListener('mousedown', () => {
         pno.style.borderColor = ("rgb(192, 192, 192)")
         abttsimg.src = ("images/piano.png")
         ititbox.textContent = ("piano")
+        doodlecat.style.display = ("none")
         abtyap.textContent = ("the piano was my first real instrument. my family put me into lessons when i was like 3 or 4 (i lowkey dont remember 😭) and it was pretty alright! i like the piano but it started to feel like a chore instead of an instrument i actively enjoyed and wanted to pursue, and i stopped taking lessons for it in 2024. i still play the piano, but not as often as i once did, and i'm not quite as good as i was before. i used to play piano in SIDEQUEST OTTAWA (until i quit 💀) and now i mostly do it for fun. i might pick it up again and do my rcm exams tho bc it'll probably be useful!!!! \n \n tl;dr im glad i learned piano and its a pretty decent instrument")
         clicksound.cloneNode(true).play();
         showSide();
@@ -166,6 +170,7 @@ bsx.addEventListener('mousedown', () => {
         bsx.style.borderColor = ("rgb(192, 192, 192)")
         abttsimg.src = ("images/bari.png")
         ititbox.textContent = ("baritone sax")
+        doodlecat.style.display = ("none")
         abtyap.textContent = ("I LOVE MY SAXY PHONE!!!!! i started playing bari sax in grade 9 after my school's bass clarinet started tweaking out on me (rip.... only lasted 2 weeks...) and i feel in love IMMEDIATELY! i started learning it, got a teacher and applied to canterbury!!! unforsch i got in for clarinet but i still get to play it for jazz this year! after all, they DO call me the BARI SAX GUY!!!!! i also got to come along with LBP's band trip last year to play with them at wonderland! i'm hopefully planning on pursuing bari sax further on in my life, and i look forward to any opportunities i get to play it! top 3 instruments of all time i swear 🤤")
         clicksound.cloneNode(true).play();
         showSide();
@@ -179,6 +184,7 @@ eup.addEventListener('mousedown', () => {
         eup.style.borderColor = ("rgb(192, 192, 192)")
         abttsimg.src = ("images/euph.png")
         ititbox.textContent = ("euphonium")
+        doodlecat.style.display = ("none")
         abtyap.textContent = ("\"it's a baby tuba 🥹\" the euphonium is pretty cool! i first started playing it in grade 7 after my trombone posture got corrected and i decided i HATED IT and RAGE QUIT!!!! anyways, turns out buttons are a LOT easier than slide positions! i played it throughout all of my band time at ND, and even became brass section leader 🤑 i ended up quitting after grade 9 though because i got kinda bored of brass and wanted to try reeds for once. i'm picking it up again for a duet, though! turns out i really havent forgotten how to play low brass!!!! i'm so goated like that yk")
         clicksound.cloneNode(true).play();
         showSide();
@@ -192,6 +198,7 @@ clt.addEventListener('mousedown', () => {
         clt.style.borderColor = ("rgb(192, 192, 192)")
         abttsimg.src = ("images/clarinet.png")
         ititbox.textContent = ("clarinet")
+        doodlecat.style.display = ("none")
         abtyap.textContent = ("clarinet. my mortal enemy. and unfortunately my program instrument at canterbury. i literally only learned it for a music project in grade 9 and had 2 weeks of experience when i got in for canterbury how did ts even happen bro 😭😭😭 anyways, i've been kinda learning to tolerate the clarinet. it isnt easy, but i've finally started getting up to speed with the rest of the band! it's nothing really special, but it IS what i go to music school to do, so i suppose that's noteworthy. haha. NOTEworthy. ha. haha. ha. clarinet is a chopped ahh instrument but if it wasnt for me playing it i wouldn't be at the best school in ottawa so yk... you win some, you lose some!")
         clicksound.cloneNode(true).play();
         showSide();
@@ -205,6 +212,7 @@ tbn.addEventListener('mousedown', () => {
         tbn.style.borderColor = ("rgb(192, 192, 192)")
         abttsimg.src = ("images/tbn.png")
         ititbox.textContent = ("trombone")
+        doodlecat.style.display = ("none")
         abtyap.textContent = ("probably my least played instrument i'd say i'm somewhat proficient at. i played it for most of grade 7 band, and was pretty alright, i guess!! again, nothing really special. i liked the trombone until i found out i had to hold it UP and then poor baby azzy couldn't handle having an instrument on her shoulder so i quit 💀 but hey! i'm coming back to it for a duet too!!! and who knows, i might have to do it for jazz band next year if i cant get a spot on the bari sax 😓")
         clicksound.cloneNode(true).play();
         showSide();
@@ -218,6 +226,7 @@ flt.addEventListener('mousedown', () => {
         flt.style.borderColor = ("rgb(192, 192, 192)")
         abttsimg.src = ("images/flt.png")
         ititbox.textContent = ("flute")
+        doodlecat.style.display = ("none")
         abtyap.textContent = ("i lowkey stole my sister's flute and learned it for funsies!!! fingerings are pretty similar to clarinet and sax so it wasn't all that hard, i guess. i'm really flat though. don't like that very much. also i cant hit high notes. Gulp! i'm glad i'm not a flautist.")
         clicksound.cloneNode(true).play();
         showSide();
@@ -231,6 +240,7 @@ bcl.addEventListener('mousedown', () => {
         bcl.style.borderColor = ("rgb(192, 192, 192)")
         abttsimg.src = ("images/bcl.png")
         ititbox.textContent = ("bass clarinet")
+        doodlecat.style.display = ("none")
         abtyap.textContent = ("i picked up bass clarinet at the start of grade 9 and genuinely started to lock in (i assumed this would be my new main instrument) and i really loved it!!! and then the keys stopped working like 3 weeks in and i had to jump ship 💔 i really hope i get another chance to play low clarinets though, they're really fun!")
         clicksound.cloneNode(true).play();
         showSide();
@@ -244,6 +254,7 @@ cbc.addEventListener('mousedown', () => {
         cbc.style.borderColor = ("rgb(192, 192, 192)")
         abttsimg.src = ("images/cbc.png")
         ititbox.textContent = ("contrabass clarinet")
+        doodlecat.style.display = ("none")
         abtyap.textContent = ("woah, was that... foreshadowing???? earlier this year i found out my school has contra clarinets and my band director said i can play one next year! im super duper super duper super duper excited!!!! not really much to say yet since the senior bass clarinet is borrowing it over the summer 😓")
         clicksound.cloneNode(true).play();
         showSide();
@@ -257,6 +268,7 @@ acc.addEventListener('mousedown', () => {
         acc.style.borderColor = ("rgb(192, 192, 192)")
         abttsimg.src = ("images/acc.png")
         ititbox.textContent = ("accordion")
+        doodlecat.style.display = ("none")
         abtyap.textContent = ("as a newfie, it is my born and bred duty to learn the accordion. i learned it in a few days for a heritage project back in grade 6 and then. barely touched it afterwards. i'm pretty alright, though!!! i learned pretty fast!!!")
         clicksound.cloneNode(true).play();
         showSide();
@@ -270,6 +282,7 @@ asx.addEventListener('mousedown', () => {
         asx.style.borderColor = ("rgb(192, 192, 192)")
         abttsimg.src = ("images/asx.png")
         ititbox.textContent = ("alto sax")
+        doodlecat.style.display = ("none")
         abtyap.textContent = ("i played this for like. 1 or 2 band rehearsals in grade 9 and then had to quit because of yearbook 💔 CAN SOMEONE EXPLAIN WHY THEY DIDNT HAVE A BAND AT ND FOR THE FIRST 8 MONTHS OF THE SCHOOL YEAR AND ONLY HAD THE BAND FOR LIKE THE LAST MONTH??? IM STILL MAD ABOUT THAT")
         clicksound.cloneNode(true).play();
         showSide();
@@ -285,6 +298,7 @@ cto.addEventListener('mousedown', () => {
         ititbox.textContent = ("contralto clarinet")
         abtyap.textContent = ("my wonderful son. this is my BABY. i am so lucky our school has basically the best model of contralto out there!!! since i cant borrow the contrabass he let me try the contralto, and its been amazing!!! its so awesome to play low niche instruments #lowreedsupremacy")
         clicksound.cloneNode(true).play();
+        doodlecat.style.display = ("flex")
         showSide();
     }
 })
@@ -296,6 +310,7 @@ tpt.addEventListener('mousedown', () => {
         tpt.style.borderColor = ("rgb(192, 192, 192)")
         abttsimg.src = ("images/tpt.png")
         ititbox.textContent = ("trumpet")
+        doodlecat.style.display = ("none")
         abtyap.textContent = ("as brass section leader, i just HAD to learn trumpet. its easy as shit guys idk what everyone's complaining about 😭 anyways, this is one of the few instruments in this section that i actually own! i plan on restoring my old trumpet to playing condition and actually learning and practicing it this time!!!")
         clicksound.cloneNode(true).play();
         showSide();
@@ -309,6 +324,7 @@ clo.addEventListener('mousedown', () => {
         clo.style.borderColor = ("rgb(192, 192, 192)")
         abttsimg.src = ("images/clo.png")
         ititbox.textContent = ("cello")
+        doodlecat.style.display = ("none")
         abtyap.textContent = ("learned it for discipline switch in grade 10. i was pretty bad at it. nuff said.")
         clicksound.cloneNode(true).play();
         showSide();
@@ -322,6 +338,7 @@ mal.addEventListener('mousedown', () => {
         mal.style.borderColor = ("rgb(192, 192, 192)")
         abttsimg.src = ("images/mal.png")
         ititbox.textContent = ("mallets")
+        doodlecat.style.display = ("none")
         abtyap.textContent = ("sometimes during lunch i'll just start playing the mallets because they're always out and theres no restrictions on who can play them!!!! it's pretty sweet and i'm not actually that bad at them!!! like i could probably do mallets for a music group if i was needed.")
         clicksound.cloneNode(true).play();
         showSide();
@@ -336,8 +353,22 @@ function hideSide() {
     sidebox.style.display = ("none")
     musicbigbox.style.left = ("0%")
     musicbigbox.style.transform = ("translateX(0px)")
+    doodlecat.style.display = ("none")
 }
 document.addEventListener("DOMContentLoaded", () => {
     let bclrotate = Math.random() * 50 - 25;
     doodlebcl.style.transform = `rotate(${bclrotate}deg)`;
+})
+doodlecat.addEventListener('mouseenter', () => {
+    doodlecat.textContent = "≽^•⩊•^≼";
+})
+doodlecat.addEventListener('mouseleave', () => {
+    doodlecat.textContent = "≽^-⩊-^≼";
+})
+doodlecat.addEventListener('mousedown', () => {
+    doodlecat.textContent = "≽^•⩊-^≼";
+    mew.preservesPitch = false;
+    mew.playbackRate = 1.2;
+    mew.play();
+    doodlecat.classList.add("sustingus")
 })
